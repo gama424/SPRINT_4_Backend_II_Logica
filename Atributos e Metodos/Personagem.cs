af014d48295388fe767b923d3e68a26953c46976
gama424 <tangodown4977@gmail.com>
2021-01-06 16:04:09 -0300

    aprenderemos o que são métodos e atributos, com a devida sintaxe aplicada em C#

---
+++ Atributos e Metodos/Personagem.cs
@@ -0,0 +1,25 @@
+namespace atributos_e_metodos
+{
+    public class Personagem
+    {
+        public string nome = "Darth Vader";
+
+        public int idade = 40;
+
+        public string equipamento = "sabre de luz";
+
+
+
+        public string Atacar(){
+            return  "destruir todos!";
+
+            
+        
+        }
+
+         public string Defender(){
+            return  "só por cima do meu cadaver!";
+
+    }
+}
+}
---
+++ Atributos e Metodos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace atributos_e_metodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Personagem objetoPersonagem1 = new Personagem();
+
+            objetoPersonagem1.nome = "darth";
+            objetoPersonagem1.idade = 99;
+            objetoPersonagem1.equipamento = "sabre de luz";
+
+
+
+                 Personagem objetoPersonagem2 = new Personagem();
+
+
+                 
+            objetoPersonagem2.nome = "luke";
+            objetoPersonagem2.idade = 45;
+            objetoPersonagem2.equipamento = "sabre de luz";
+
+            
+            Console.WriteLine($"Personagem 1 = {objetoPersonagem1.nome} - {objetoPersonagem1.Atacar()}");
+            
+            Console.WriteLine($"Personagem 2 = {objetoPersonagem2.nome} - {objetoPersonagem1.Defender()}");
+    }
+}
+}
