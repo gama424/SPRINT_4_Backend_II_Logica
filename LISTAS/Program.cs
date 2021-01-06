9a946194f76aafde5e034598c9aea712efbfefb7
gama424 <tangodown4977@gmail.com>
2021-01-06 16:57:22 -0300

    aprenderemos como usar listas em C#, aplicando diretamente em objetos criados

---
+++ LISTAS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using LISTAS.classes;
+
+namespace LISTAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Produto> produtos = new List<Produto>();
+
+            Produto cafe = new Produto(2, "Café pequeno", 2.5f);
+
+            produtos.Add(new Produto(1, "pao que o diabo amassou", 6.6f));
+           produtos.Add(cafe);
+           produtos.Add(new Produto(3, "mistao quente", 6.0f));
+           produtos.Add(new Produto(4, "tubaina", 7.9f));
+
+
+
+            foreach (Produto item in produtos)
+            {
+                    Console.WriteLine($"item codigo{item.Codigo} Nome {item.Nome} Preço {item.Preco}");
+            }
+        }
+    }
+}
---
+++ LISTAS/classes/Produto.cs
@@ -0,0 +1,23 @@
+namespace LISTAS.classes
+
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+
+           public string Nome { get; set; }
+
+              public string Preco { get; set; }
+        
+
+        public Produto(){
+
+        }
+                public Produto(int _codigo, string _nome, float _preco){
+                    this.Codigo = _codigo;
+                    this.Nome = _nome;
+                    this.Preco = _preco;
+
+                }
+    }
+}
